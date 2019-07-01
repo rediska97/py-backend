4db4c385cc550772e7d2b965471cb943d1a055ae
Andrei <boronciuc.andrei@gmail.com>
2019-07-01 23:25:26 +0300

    add schema

--- app.py
+++ app.py
@@ -1,22 +1,84 @@
 # Imports
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
+
+from flask_graphql import GraphQLView
+
 # app initialization
 app = Flask(__name__)
 app.debug = True
+
+# Imports
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Configs
-# TO-DO
+POSTGRES = {
+    'user': 'postgres',
+    'pw': '1234',
+    'db': 'graphql',
+    'host': 'localhost',
+    'port': '5432',
+}
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/graphql'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
 # Modules
+db = SQLAlchemy(app)
 # TO-DO
+
 # Models
-# TO-DO
+class User(db.Model):
+    __tablename__ = 'users'
+    uuid = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), index=True, unique=True)
+    posts = db.relationship('Post', backref='author')
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+class Post(db.Model):
+    __tablename__ = 'posts'
+    uuid = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(256), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.uuid'))
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
 # Schema Objects
-# TO-DO
-# Routes
-# TO-DO
+class PostObject(SQLAlchemyObjectType):
+    class Meta:
+        model = Post
+        interfaces = (graphene.relay.Node, )
+class UserObject(SQLAlchemyObjectType):
+   class Meta:
+       model = User
+       interfaces = (graphene.relay.Node, )
+class Query(graphene.ObjectType):
+    node = graphene.relay.Node.Field()
+    all_posts = SQLAlchemyConnectionField(PostObject)
+    all_users = SQLAlchemyConnectionField(UserObject)
+schema = graphene.Schema(query=Query)
+
+
 @app.route('/')
 def index():
     return '<p> Hello World</p>'
 
+app.add_url_rule(
+    '/graphql',
+    view_func=GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True # for having the GraphiQL interface
+    )
+)
 
 if __name__ == '__main__':
     app.run()
+
